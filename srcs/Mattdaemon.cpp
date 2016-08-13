1b82603c49effa2bee7c908e0e162e4931ea96c5
Fabien TAFFOREAU <ftaffore@e1r11p8.42.fr>
2016-08-13 18:46:23 +0200

    test without son

--- srcs/Mattdaemon.cpp
+++ srcs/Mattdaemon.cpp
@@ -41,27 +41,32 @@ Mattdaemon::Mattdaemon(const Tintin_reporter *tintin_reporter) : _log(tintin_rep
 Mattdaemon::~Mattdaemon(void) {
 	
 	std::cout << "STAR ENDEND" << std::endl;
-	for (std::list<Fd *>::iterator it = this->_fds.begin(); it != this->_fds.end();) {
-		std::cout << "LOOP CLIENT: " << *it << std::endl;
-		if ((*it)->type == FD_CLIENT) {
-			close((*it)->fd);
-			delete *it;
-			this->_fds.erase(it++);
+	if (this->_fds != NULL) {
+		
+		for (std::list<Fd *>::iterator it = this->_fds.begin(); it != this->_fds.end();) {
+			std::cout << "LOOP CLIENT: " << *it << std::endl;
+			if ((*it)->type == FD_CLIENT) {
+				close((*it)->fd);
+				delete *it;
+				this->_fds.erase(it++);
+			}
 		}
-	}
-	std::cout << "ENDEND >> " << std::endl;
-	for (std::list<Fd *>::iterator it = this->_fds.begin(); it != this->_fds.end();) {
-		std::cout << "LOOP SERVER: " << *it << " : " << (*it)->fd << std::endl;
-		if ((*it)->type == FD_SERVER) {
-			close((*it)->fd);
-			delete *it;
-			this->_fds.erase(it++);
+		std::cout << "ENDEND >> " << std::endl;
+		for (std::list<Fd *>::iterator it = this->_fds.begin(); it != this->_fds.end();) {
+			std::cout << "LOOP SERVER: " << *it << " : " << (*it)->fd << std::endl;
+			if ((*it)->type == FD_SERVER) {
+				close((*it)->fd);
+				delete *it;
+				this->_fds.erase(it++);
+			}
 		}
+		std::cout << "ENDEND" << std::endl;
+	    this->_fds.clear();
 	}
-	std::cout << "ENDEND" << std::endl;
-    this->_fds.clear();
-	std::cout << "ENDEND" << std::endl;
-    this->_msgs.clear();
+	if (this->_mgs != NULL) {
+		std::cout << "ENDEND" << std::endl;
+    	this->_msgs.clear();
+	}
 	std::cout << "ENDEND" << std::endl;
 	return ;
 }
